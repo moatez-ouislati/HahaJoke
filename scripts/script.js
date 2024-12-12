84ad0576acf29ec0d0d07dc451ddf30e4ec89dfa
moatez-ouislati <m.ouesleti1305@myu.universitecentrale.tn>
2024-12-12 19:56:56 +0100

    Simple commit

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const JokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = () => {
+    JokeContainer.classList.remove("fade");
+    fetch(url)
+    .then(data => data.json())
+    .then(item => {
+        JokeContainer.textContent = `${item.joke}`;
+        JokeContainer.classList.add("fade");
+    });
+}
+btn.addEventListener("click", getJoke);
+getJoke();
